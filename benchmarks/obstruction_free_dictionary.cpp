a7288fc016cda79a1129baf7e35b9e7518c7da05
Chase Conklin <chase@conklins.net>
2017-04-20 19:37:48 -0400

    add dictionary benchmark

--- benchmarks/obstruction_free_dictionary.cpp
+++ benchmarks/obstruction_free_dictionary.cpp
@@ -1,47 +1,101 @@
 #include <iostream>
+#include <thread>
+#include <atomic>
+#include <vector>
+#include <chrono>
 #include <obstruction_free_dictionary.hpp>
+
+void bm_func(nonblocking::obstruction_free_dictionary& dict, int ops, int reads, int writes, int deletes, std::atomic<bool>& start)
+{
+    while (!start.load());
+    for (unsigned int i = 0; i < ops; ++i)
+    {
+        for (unsigned int j = 0; j < writes; ++j)
+        {
+            dict.set(static_cast<unsigned int>(rand()) % 2048, j);
+        }
+        for (unsigned int j = 0; j < reads; ++j)
+        {
+            dict.lookup(static_cast<unsigned int>(rand()) % 2048);
+        }
+        for (unsigned int j = 0; j < deletes; ++j)
+        {
+            dict.erase(static_cast<unsigned int>(rand()) % 2048);
+        }
+    }
+}
+
+void calibrate(int ops, std::atomic<bool>& start)
+{
+    while (!start.load());
+    volatile unsigned int x;
+    for (unsigned int i = 0; i < ops; ++i)
+    {
+        x = static_cast<unsigned int>(rand()) % 2048;
+    }
+}
+
+double benchmark(int t, int ops, int reads, int writes, int deletes)
+{
+    std::atomic<bool> start;
+    std::vector<std::thread> threads;
+    std::vector<std::thread> cal_threads;
+    nonblocking::obstruction_free_dictionary obmap(32768);
+
+    // Calibration run
+
+    start.store(false);
+
+    for (int i = 0; i < t; ++i)
+    {
+        cal_threads.push_back(std::thread(calibrate, ops*(reads + writes + deletes), std::ref(start)));
+    }
+    auto cal_start = std::chrono::high_resolution_clock::now();
+
+    start.store(true);
+
+    for (auto& thread : cal_threads)
+    {
+        thread.join();
+    }
+    auto cal_stop = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> cal_time = cal_stop - cal_start;
+
+    start.store(false);
+
+    // Actual Run
+    for (int i = 0; i < t; ++i)
+    {
+        threads.push_back(std::thread(bm_func, std::ref(obmap), ops, reads, writes, deletes, std::ref(start)));
+    }
+
+    auto run_start = std::chrono::high_resolution_clock::now();
+
+    start.store(true);
+
+    for (auto& thread : threads)
+    {
+        thread.join();
+    }
+
+    auto run_stop = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> run_time = run_stop - run_start;
+
+    return run_time.count() - cal_time.count();
+}
 
 int main(int argc, const char *argv[])
 {
-    nonblocking::obstruction_free_dictionary obmap(32);
-    auto look = obmap.lookup(5);
-    if (std::get<0>(look))
+    auto ops = 1000000;
+    auto reads = 8;
+    auto writes = 1;
+    auto deletes = 1;
+    std::vector<int> threads = {1, 2, 3, 4, 6, 8, 12, 16, 32, 64, 96, 128};
+    for (auto t : threads)
     {
-        std::cout << "(fail) empty map contains 5 (value = " << std::get<1>(look) << ")" << std::endl;
-    }
-    else
-    {
-        std::cout << "empty map does not contain 5" << std::endl;
-    }
-    obmap.set(5, 11);
-    look = obmap.lookup(5);
-    if (std::get<0>(look))
-    {
-        std::cout << "map contains 5 (value = " << std::get<1>(look) << ")" << std::endl;
-    }
-    else
-    {
-        std::cout << "(fail) map does not contain 5" << std::endl;
-    }
-    obmap.set(5, 18);
-    look = obmap.lookup(5);
-    if (std::get<0>(look))
-    {
-        std::cout << "map contains 5 (value = " << std::get<1>(look) << ")" << std::endl;
-    }
-    else
-    {
-        std::cout << "(fail) map does not contain 5" << std::endl;
-    }
-    std::cout << obmap.erase(5) << std::endl;
-    look = obmap.lookup(5);
-    if (std::get<0>(look))
-    {
-        std::cout << "(fail) empty map contains 5 (value = " << std::get<1>(look) << ")" << std::endl;
-    }
-    else
-    {
-        std::cout << "empty map does not contain 5" << std::endl;
+        auto runtime = benchmark(t, ops, reads, writes, deletes);
+        std::cout << "[" << t << "] Microseconds / Op: " << 1000000.0 * runtime / static_cast<double>(ops) << std::endl;
     }
     return 0;
 }
