01aaacd03d25c582bc59b380114a93fc004bd756
Chase Conklin <chase@conklins.net>
2017-04-20 20:20:05 -0400

    add perf benchmarks

--- benchmarks/obstruction_free_dictionary.cpp
+++ benchmarks/obstruction_free_dictionary.cpp
@@ -25,45 +25,12 @@ void bm_func(nonblocking::obstruction_free_dictionary& dict, int ops, int reads,
     }
 }
 
-void calibrate(int ops, std::atomic<bool>& start)
-{
-    while (!start.load());
-    volatile unsigned int x;
-    for (unsigned int i = 0; i < ops; ++i)
-    {
-        x = static_cast<unsigned int>(rand()) % 2048;
-    }
-}
-
 double benchmark(int t, int ops, int reads, int writes, int deletes)
 {
     std::atomic<bool> start;
     std::vector<std::thread> threads;
-    std::vector<std::thread> cal_threads;
     nonblocking::obstruction_free_dictionary obmap(32768);
 
-    // Calibration run
-
-    start.store(false);
-
-    for (int i = 0; i < t; ++i)
-    {
-        cal_threads.push_back(std::thread(calibrate, ops*(reads + writes + deletes), std::ref(start)));
-    }
-    auto cal_start = std::chrono::high_resolution_clock::now();
-
-    start.store(true);
-
-    for (auto& thread : cal_threads)
-    {
-        thread.join();
-    }
-    auto cal_stop = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> cal_time = cal_stop - cal_start;
-
-    start.store(false);
-
-    // Actual Run
     for (int i = 0; i < t; ++i)
     {
         threads.push_back(std::thread(bm_func, std::ref(obmap), ops, reads, writes, deletes, std::ref(start)));
@@ -82,16 +49,16 @@ double benchmark(int t, int ops, int reads, int writes, int deletes)
 
     std::chrono::duration<double> run_time = run_stop - run_start;
 
-    return run_time.count() - cal_time.count();
+    return run_time.count();
 }
 
 int main(int argc, const char *argv[])
 {
-    auto ops = 1000000;
+    auto ops = 10000;
     auto reads = 8;
     auto writes = 1;
     auto deletes = 1;
-    std::vector<int> threads = {1, 2, 3, 4, 6, 8, 12, 16, 32, 64, 96, 128};
+    std::vector<int> threads = {1, 2, 3, 4, 6, 8, 12, 16, 24, 32, 48, 64, 96, 128};
     for (auto t : threads)
     {
         auto runtime = benchmark(t, ops, reads, writes, deletes);
